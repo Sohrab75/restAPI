99cd0bbde86e4bf758b4f4632caa34aeb7fdc970
Md Sohrab Ansari <ansarisohrab75@gmail.com>
2023-03-06 15:06:41 +0530

    cors

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 require("dotenv").config();
 const express = require('express');
+const cors=require("cors");
 const app = express();
 const connectDb = require('./db/connect')
 const products_route =require('./Routes/Routes')
 const PORT = process.env.PORT || 8000;
+
+
+
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions));
+
 
 app.get('/', (req, res) => {
     res.send('Hello World! this is Live from express');
